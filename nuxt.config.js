93257f2d6ae6d70d1c59a2d3566773a290298de6
dhymasriyanto <gojin003@gmail.com>
2021-12-13 15:15:41 +0700

    import tabler-icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/tabler'
+    '@/plugins/tabler',
+    '@/plugins/tabler-icons'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
