a0e4e06c2eb6635e35fcb31a226be113cda78198
dhymasriyanto <gojin003@gmail.com>
2021-12-22 04:20:51 +0700

    default and package.json update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <AppHeader />
+    <Sidebar />
     <Nuxt />
   </div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue'
+import Sidebar from '../components/Sidebar.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    Sidebar
   }
 }
 </script>
